4e9c698cf8943bec760b85a021a4a61025ef1c6e
zbr11 <98388911+zbr11@users.noreply.github.com>
2022-05-23 23:28:24 +0800

    Add files via upload

---
+++ code/EDS/get_samples.py
@@ -0,0 +1,20 @@
+import numpy as np
+import csv
+def _read_tsv(input_file):
+    with open(input_file, "r", encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter="\t")
+        lines = []
+        for line in reader:
+            lines.append(line)
+        return lines
+lines = _read_tsv("./train_new.tsv")
+f=open("index_sst2.txt","r")
+idx_list = []
+for line in f.readlines():
+    idx_list.append(int(line))
+f=open("./train_select_new_sst2_957.tsv","w")
+for idx in idx_list:
+    f.write("\t".join(lines[idx]))
+    f.write("\n")
+
+
